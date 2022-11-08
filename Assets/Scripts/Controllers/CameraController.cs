06c692c389dbfbdff92547b77a3c0e53bb519c95
juliMAB <julian6aguirre@hotmail.com>
2022-11-08 16:13:54 -0300

    Add camera move system.

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region EXPOSED_FIELDS
+    public enum CAMERA_MODE
+    {
+        NORMAL,
+        FIXED,
+        LATE
+    }
+
+    [SerializeField] private float speed = 1.0f;
+
+    [SerializeField] private CAMERA_MODE camera_mode = CAMERA_MODE.NORMAL;
+    #endregion
+
+    #region UNITY_CALLS
+    private void LateUpdate()
+    {
+        if (camera_mode == CAMERA_MODE.LATE)
+        {
+            UpdateInputsCamera();
+        }
+    }
+    private void Update()
+    {
+        if (camera_mode == CAMERA_MODE.NORMAL)
+        {
+            UpdateInputsCamera();
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (camera_mode == CAMERA_MODE.FIXED)
+        {
+            UpdateInputsCamera();
+        }
+    }
+    #endregion
+
+    #region PRIVATE_METHODS
+    private void UpdateInputsCamera()
+    {
+        float xvalue = Input.GetAxis("Horizontal");
+        float zvalue = Input.GetAxis("Depth");
+        float yvalue = Input.GetAxis("Vertical");
+
+        Vector3 newMove = new Vector3(xvalue, yvalue, zvalue);
+        transform.position = newMove*speed*Time.deltaTime + transform.position;
+    }
+
+    #endregion
+
+}

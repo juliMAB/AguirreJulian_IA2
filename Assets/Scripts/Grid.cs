39e67fde722157e336602608008fad695554ed5c
juliMAB <julian6aguirre@hotmail.com>
2022-11-07 23:18:24 -0300

    Add basic grid.

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UnityEngine;
+
+public class Grid {
+
+    public event EventHandler<OnGridValueChangedEventArgs> OnGridValueChanged;
+    public class OnGridValueChangedEventArgs : EventArgs {
+        public int x;
+        public int y;
+    }
+
+    private int width;
+    private int height;
+    private float cellSize;
+    private Vector3 originPosition;
+    private int[,] gridArray;
+
+    public Grid(int width, int height, float cellSize, Vector3 originPosition) {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        gridArray = new int[width, height];
+
+    }
+
+    public int GetWidth() {
+        return width;
+    }
+
+    public int GetHeight() {
+        return height;
+    }
+
+    public float GetCellSize() {
+        return cellSize;
+    }
+
+    public Vector3 GetWorldPosition(int x, int y) {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y) {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    public void SetValue(int x, int y, int value) {
+        if (x >= 0 && y >= 0 && x < width && y < height) {
+            gridArray[x, y] = value;
+            if (OnGridValueChanged != null) OnGridValueChanged(this, new OnGridValueChangedEventArgs { x = x, y = y });
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y) {
+        if (x >= 0 && y >= 0 && x < width && y < height) {
+            return gridArray[x, y];
+        } else {
+            return 0;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
+    public void drawGizmos()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            Gizmos.DrawLine(new Vector3(0,i), new Vector3(width, i));
+        }
+        for (int i = 0; i < height; i++)
+        {
+            Gizmos.DrawLine(new Vector3(i, 0), new Vector3(i, height));
+        }
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+
+    [SerializeField, Tooltip("The size of the grid on X coordinate."), Range(100, 300)] private int sizeGridX = 100;
+    [SerializeField, Tooltip("The size of the grid on Y coordinate."), Range(100, 300)] private int sizeGridY = 100;
+
+    private Grid grid;
+
+    private void Start() {
+        grid = new Grid(sizeGridX, sizeGridY, 10f, new Vector3(0, 0));
+    }
+
+    private void Update() {
+        
+    }
+    private void OnDrawGizmos()
+    {
+        if (grid != null)
+        {
+            grid.drawGizmos();
+        }
+    }
+}
